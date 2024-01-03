1f162cdf77b9469daac63bc66965405bad87e5f9
AsarNaufil <naufilasar.dev@gmail.com>
2024-01-03 18:33:45 +0530

    Added Calculator Page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { DashboardPage, LoginPage } from './components';
+import CalculatorPage from './components/Calculator/CalculatorPage';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path='/dashboard' element={<DashboardPage />} />
+        <Route path='/calculator' element={<CalculatorPage />} />
       </Routes>
     </BrowserRouter>
   )
