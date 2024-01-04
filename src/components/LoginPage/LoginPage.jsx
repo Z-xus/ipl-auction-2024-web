4e3192b9991a77fb6b6b0a982413fd23003d5aef
Asim Shah <asimshah2111@gmail.com>
2024-01-04 05:37:36 +0530

    started working on login page

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,13 +1,24 @@
-// LoginPage.jsx
-// import React from 'react';
-
+import React from 'react';
+import "./LoginPage.css"
 const LoginPage = () => {
   return (
-    <div className="bg-purple-700">
-      <h1 className="text-red-400">Hello World</h1>
+    <div className="main flex justify-center items-center h-screen">
+      <div className="login flex flex-col justify-center items-center h-96 w-96 rounded-3xl bg-white bg-opacity-50 border-black p-4">
+        <h1 className="font-bold text-black text-3xl mb-10">Login</h1>
+        <form className="flex flex-col items-center">
+          <input type="text" placeholder='Username' className="input-field mb-10 w-80 rounded-md" />
+          <input type="password" placeholder='Password' className="input-field mb-10 w-80 rounded-md" />
+          <input type="text" placeholder='Slot' className="input-field mb-10 w-80 rounded-md" />
+          <button type="submit" className="btn-primary mb-10 bg-blue-800 w-44 rounded-md">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
+
+
 
 export default LoginPage;
 
