948d0d3c67ec713cb543af8bb02380b68fa32296
AsarNaufil <naufilasar.dev@gmail.com>
2024-01-07 10:22:59 +0530

    Implemented dragging.

--- src/components/Calculator/CalculatorPage.jsx
+++ src/components/Calculator/CalculatorPage.jsx
@@ -1,32 +1,90 @@
-// import React from 'react';
+import { useState } from 'react';
+import NavBar from "../Utils/Navbar"
 import "./style.css";
+import { RadioBox, CardContainer, Box, Card } from "./Utils.jsx";
+import  playerData  from "./assets/player";
 
+
+// dummy user.
 const user = {
     points: 144,
-    no_of_cards: 0
+    no_of_cards: 0,
+    avail_players: 5
 };
 
+// TODONEWS1: join <Card /> with the json data.
+// 2: take up the data from cards and add up to the total.
+// TODO0: Create drag and drop functionality using html5 api.
+// 0.1: create a state to carry information.
+// TODO1: Hold the card in the placeholder. 
+// TODO2: Add them into the user's total.
+// TODO3: Calculate the summation correctly using the user properties and selected options.
+
 const Calculator = () => {
+
+    const total_pts = 0;
+    // To save the cards which are dragged into placeholder.
+    const [playerCards, setPlayerCards] = useState([]);
+
+
+    const [selectedBox, setSelectedBox] = useState(null);
+    const [selectedRadioBox, setSelectedRadioBox] = useState(null);
+    const handleBoxSelect = (boxId) => {
+        setSelectedBox(boxId);
+    };
+    const handleRadioBoxSelect = (boxId) => {
+        setSelectedRadioBox(boxId);
+    };
+
+    const handleOnDrop = (e) => {
+        let _data = e.dataTransfer.getData("Card");
+        console.log("type: ", _data);
+        console.log(playerCards);
+        _data = JSON.parse(_data);
+        // TODO : extract the data and add to the total
+        setPlayerCards([...playerCards, _data]);
+    };
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+    };
+
     return (
         <div className="calculator">
-            <div className="main-title flex justify-between px-4 py-4">
+            <NavBar />
+            <div className="main-title flex justify-between px-4 py-4 items-center">
                 <div className="total-points text-2xl inline py-4 px-6">
                     Total Points: {user.points}
                 </div>
-                <h1 className="font-bold text-4xl inline">Calculator</h1>
-                <div className="btns inline space-x-3">
-                    <button className="btn py-4 px-6 text-2xl rounded bat-btn">Bat</button>
-                    <button className="btn py-4 px-6 text-2xl rounded ball-btn">Ball</button>
+                <h1 className="font-bold uppercase underline text-4xl inline">Calculator</h1>
+                <div className="btns flex flex-row gap-3">
+                    {
+                        // <button className="btn py-4 px-6 text-2xl rounded bat-btn">Bat</button>
+                        // <button className="btn py-4 px-6 text-2xl rounded ball-btn">Ball</button>
+                    }
+                    <RadioBox id={1} label="Bat" isSelected={selectedRadioBox === 1} onSelect={handleRadioBoxSelect} />
+                    <RadioBox id={2} label="Bowl" isSelected={selectedRadioBox === 2} onSelect={handleRadioBoxSelect} />
                 </div>
             </div>
             <div className="drag-in-container flex justify-evenly">
-                <div className="ppl-cont drag-in flex justify-center items-center"><span className="text-xl">PPL:</span></div>
-                <div className="mo-cont drag-in flex justify-center items-center "><span className="text-xl">MO:</span></div>
-                <div className="dth-cont drag-in flex justify-center items-center"><span className="text-xl">DTH:</span></div>
+                <Box id={1} label="PPL:" isSelected={selectedBox === 1} onSelect={handleBoxSelect} />
+                <Box id={2} label="MO:" isSelected={selectedBox === 2} onSelect={handleBoxSelect} />
+                <Box id={3} label="DTH:" isSelected={selectedBox === 3} onSelect={handleBoxSelect} />
             </div>
-            <div className="card-placeholder h-48 flex justify-center items-center my-5">
-                { !user.no_of_cards && <span className="placeholder-text text-xl mx-4">Placeholder for cards</span> }
-            </div>  
+            <div className="card-placeholder h-40 flex justify-center items-center my-10"
+                onDrop={handleOnDrop} onDragOver={handleDragOver}
+            >
+                {!playerCards.length && <span className="placeholder-text text-xl mx-4 flex gap-2">Drop your cards here!</span>}
+                {
+                    playerCards.map((data, index) => (
+                        // <div className="dropped" key={index}>
+                        //     {data.playerName}
+                        // </div>
+                        <Card key={index} data={data} style={{tansform : 'scale(0.9)'}} />
+                    ))
+                }
+            </div>
+            <CardContainer cardData={playerData} />
         </div>
     );
 };
---
+++ src/components/Calculator/Utils.jsx
@@ -0,0 +1,69 @@
+
+export const Box = ({ id, label, isSelected, onSelect, children }) => {
+    const handleClick = () => {
+        onSelect(id);
+    };
+
+    return (
+        <div className={`flex justify-center items-center text-xl cursor-pointer drag-in ${isSelected ? 'highlight' : ''}`} onClick={handleClick}>
+            <p>{label}</p>
+            {children}
+        </div>
+    );
+};
+
+export const Card = ({ data }) => {
+    const handleOnDrag = (e) => {
+        // if (!isDraggable) e.preventDefault(); // FIXME : cant stop dragging even after being put in placeholder.
+        e.dataTransfer.setData("Card", JSON.stringify(data));
+        console.log("ok.")
+    };
+    return (
+        <div
+            draggable
+            onDragStart={(e)=> handleOnDrag(e, 'Card')}
+            className='w-32 h-40 m-1 rounded bg-sky-900 flex flex-col justify-center cursor-pointer'>
+            <h1 className="text-center py-2">{data.playerName}</h1>    
+            <div className="flex flex-row justify-evenly">
+                <div className="flex flex-col text-center">
+                    <span>Bat</span>
+                    <span>{data.bat_ppl}</span>
+                    <span>{data.bat_mo}</span>
+                    <span>{data.bat_dth}</span>
+                </div>
+                <div className="flex flex-col text-center">
+                    <span>Bowl</span>
+                    <span>{data.bow_ppl}</span>
+                    <span>{data.bow_mo}</span>
+                    <span>{data.bow_dth}</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+
+// We don't need this container afterall. Just used once
+export const CardContainer = ({ cardData }) => {
+    return (
+        <ul className="flex justify-center items-center list-none gap-2">
+            {!cardData.length === 0 && <span className="avail-player-text text-xl mx-4">Available Players Are Displayed here.</span>}
+            {
+                cardData.map((card, index) => (
+                    <Card key={index} data={card} />
+                ))}
+        </ul>
+    );
+};
+
+export const RadioBox = ({ id, label, isSelected, onSelect }) => {
+    const handleRadioClick = () => {
+        onSelect(id);
+    };
+
+    return (
+        <div className={`w-28 cursor-pointer btn m-0 py-4 px-6 rounded bat-btn text-center text-2xl ${isSelected ? 'highlight-btn  ' : ''}`} onClick={handleRadioClick}>
+            {label}
+        </div>
+    );
+};
