6374515b8126d95ea726c9d5fdd757360c0d6ded
SoulDev07 <mayankravariya@gmail.com>
2024-01-18 17:39:37 +0530

    Add Logos to Login page

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -18,41 +18,21 @@ Input.propTypes = {
 
 const LoginPage = () => (
   <div className="main">
+    {/* Logo Images */}
+    <img className='absolute h-32 top-0 right-0' src='/images/logo/oculus.png' />
+    <img className='absolute h-28 top-16 left-105 z-10' src='/images/logo/iplauction.png' />
+
+    {/* Login Form */}
     <div className="login-container">
-      <h1 className="login-title">Login</h1>
+      <h1 className="login-title"> Login </h1>
       <form className="login-form">
         <Input type="text" name="Username" placeholder="Username" label="username" />
         <Input type="password" name="Password" placeholder="Password" label="password" />
         <Input type="text" name="Slot" placeholder="Slot No." label="slot" />
-        <button type="submit" className="login-button">
-          Login
-        </button>
+        <button type="submit" className="login-button"> Login </button>
       </form>
     </div>
   </div>
 );
 
 export default LoginPage;
-
-/*
-   <div className="box">
-      <div className="group">
-        <div className="text-wrapper">Login</div>
-        <div className="overlap">
-          <div className="div">Sign in</div>
-        </div>
-        <div className="group-2">
-          <div className="overlap-group">
-            <div className="text-wrapper-2">username@gmail.com</div>
-          </div>
-          <div className="text-wrapper-3">Email</div>
-          <div className="overlap-group-2">
-            <div className="text-wrapper-4">Password</div>
-            <img className="clarity-eye-hide" alt="Clarity eye hide" src="clarity-eye-hide-line.svg" />
-          </div>
-          <div className="text-wrapper-5">Password</div>
-          <div className="text-wrapper-6">Forgot Password?</div>
-        </div>
-      </div>
-    </div>
-*/
