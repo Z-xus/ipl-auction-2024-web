8f3c46441029b157736d45eb3815890a5cea8382
unknown <ninadmaadhavi13@gmail.com>
2024-01-27 15:18:18 +0530

    minor changes

--- src/components/LeaderboardPage/LeaderboardPage.jsx
+++ src/components/LeaderboardPage/LeaderboardPage.jsx
@@ -15,16 +15,16 @@ const teamsData = [
 
 const LeaderboardPage = () => {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-screen bg">
       <Navbar style={{ backdropFilter: 'blur(12.5px)' }} />
       <div className="flex flex-col items-center justify-around bg flex-grow" style={{ backdropFilter: 'blur(12.5px)' }}>
-        <h2 className="title"> LEADERBOARD </h2>
-        <div className="p-2">
+        <h2 className="title text-7xl my-16"> LEADERBOARD </h2>
+        <div className="p-2 m-0">
           {teamsData.map((team, index) => (
-            <div key={index} className="flex items-center justify-between mb-1 min-h-12 text-center">
-              <div className="text-lg font-extrabold mx-1 min-w-6 min-h-8 p-1 bg-yellow-500 text-black">{team.position}</div>
-              <div className="text-lg mx-1 font-bold grow min-h-8 p-1 px-2 tilted-border">{team.name}</div>
-              <div className="text-lg font-extrabold mx-2 min-w-6 min-h-8 p-1 transform -skew-x-12 bg-yellow-500 text-black">{team.points}</div>
+            <div key={index} className="flex items-center justify-between mb-6 min-h-12 text-center">
+              <div className="text-3xl font-extrabold mx-1 min-w-8 min-h-8 p-1 px-2 bg-yellow-500 text-black">{team.position}</div>
+              <div className="text-3xl mx-1 font-extrabold grow min-h-8 p-1 px-4 tilted-border">{team.name}</div>
+              <div className="text-3xl font-extrabold mx-2 min-w-8 min-h-8 p-1 px-2 transform -skew-x-12 bg-yellow-500 text-black">{team.points}</div>
             </div>
           ))}
         </div>
