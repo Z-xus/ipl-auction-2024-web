4f509f765097e09e9a31b1ea279d1e793f40bbd2
SoulDev07 <mayankravariya@gmail.com>
2024-02-08 10:56:37 +0530

    Store session data when user logins

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,11 +1,14 @@
 // import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import './LoginPage.css';
 
-const Input = ({ type, name, placeholder, label }) => (
+const Input = ({ type, name, placeholder, label, onChange }) => (
   <div className="input-group">
     <label className="input-label" htmlFor={label}>{name}</label>
-    <input className="input-field" type={type} id={label} placeholder={placeholder} />
+    <input className="input-field" type={type} id={label} placeholder={placeholder} onChange={onChange}/>
   </div>
 );
 
@@ -13,26 +16,54 @@ Input.propTypes = {
   type: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func
 };
 
-const LoginPage = () => (
-  <div className="main">
-    {/* Logo Images */}
-    <img className='absolute h-32 top-0 right-0' src='/images/logo/oculus.png' />
-    <img className='absolute h-28 top-16 left-105 z-10' src='/images/logo/iplauction.png' />
+const LoginPage = () => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [slot, setSlot] = useState();
+  const navigate = useNavigate();
 
-    {/* Login Form */}
-    <div className="login-container">
-      <h1 className="login-title"> Login </h1>
-      <form className="login-form">
-        <Input type="text" name="Username" placeholder="Username" label="username" />
-        <Input type="password" name="Password" placeholder="Password" label="password" />
-        <Input type="text" name="Slot" placeholder="Slot No." label="slot" />
-        <button type="submit" className="login-button"> Login </button>
-      </form>
+  const submitForm = async () => {
+    const userDetails = {
+      username: username,
+      password: password,
+      slot: slot
+    };
+
+    const { data } = await axios.post(`${import.meta.env.VITE_SERVERURL}/login`, userDetails, { headers: { "Content-Type": "application/json" } });
+
+    if (data.user) {
+      localStorage.clear();
+      localStorage.setItem("username", data.user.username);
+      localStorage.setItem("team", data.user.teamName);
+      localStorage.setItem("slot", slot);
+      navigate("/dashboard");
+    } else {
+      alert(data.message);
+    }
+  }
+
+  return (
+    <div className="main">
+      {/* Logo Images */}
+      <img className='absolute h-32 top-0 right-0' src='/images/logo/oculus.png' />
+      <img className='absolute h-28 top-16 left-105 z-10' src='/images/logo/iplauction.png' />
+
+      {/* Login Form */}
+      <div className="login-container">
+        <h1 className="login-title"> Login </h1>
+        <div className="login-form">
+          <Input type="text" name="Username" placeholder="Username" label="username" onChange={e => setUsername(e.target.value)} />
+          <Input type="password" name="Password" placeholder="Password" label="password" onChange={e => setPassword(e.target.value)} />
+          <Input type="text" name="Slot" placeholder="Slot No." label="slot" onChange={e => setSlot(e.target.value)} />
+          <button type="submit" className="login-button" onClick={submitForm}> Login </button>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default LoginPage;
