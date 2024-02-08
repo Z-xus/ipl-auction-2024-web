975cd05a55b806b4f41714258f172b9ec651be39
unknown <ninadmaadhavi13@gmail.com>
2024-02-08 23:02:16 +0530

    dashboard crashed,naufil pls help

--- src/components/SpectatePage/SpectatePage.jsx
+++ src/components/SpectatePage/SpectatePage.jsx
@@ -2,34 +2,30 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { DashboardPage } from '../'
-
-
-const dummyTeam = {
-  "username": "user1",
-  "password": "password1",
-  "slot": 1,
-  "teamName": "CSK",
-  "budget": 765678921,
-  "score": 78,
-  "players": [],
-  "powercards": [
-    { name: "focus fire", isUsed: false },
-    { name: "double right to match", isUsed: false },
-    { name: "god's eye", isUsed: true },
-    { name: "right to match", isUsed: true },
-    { name: "silent reserve", isUsed: false },
-    { name: "stealth bid", isUsed: true }
-  ]
-}
+import axios from 'axios';
 
 const SpectatePage = () => {
   
   const { teamName } = useParams();
+  const slot = localStorage.getItem('slot');
   const [teamDetails, setTeamDetails] = useState(null);
 
   useEffect(() => {
-    setTeamDetails(dummyTeam);
-  }, [teamName])
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3000/spectate/${teamName}/${slot}`);
+        // const response = await axios.get(`http://localhost:3000/spectate/MI/2`);
+        const data = response.data;
+        console.log(data);
+        setTeamDetails(data.newUser);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [teamName,slot]);
+
 
   return <DashboardPage teamDetails={teamDetails} />;
 };
