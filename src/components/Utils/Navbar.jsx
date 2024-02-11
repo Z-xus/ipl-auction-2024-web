124b3a8edeac2295fc7a5da2532674b64165b1a0
SoulDev07 <mayankravariya@gmail.com>
2024-02-11 11:18:57 +0530

    improve spectate ui

--- src/components/Utils/Navbar.jsx
+++ src/components/Utils/Navbar.jsx
@@ -6,6 +6,12 @@ import Drawer from '@mui/material/Drawer';
 import Popup from "./Popup";
 import "./Navbar.css";
 
+const TEAMS = {
+  csk: "Chennai Super Kings", dc: "Delhi Capitals", gt: "Gujarat Titans",
+  kkr: "Kolkata Knight Riders", lsg: "Lucknow Supergiants", mi: "Mumbai Indians", pbks: "Punjab Kings",
+  rcb: "Royal Challengers Bangalore", rr: "Rajasthan Royals", srh: "Sunrisers Hyderabad"
+};
+
 const NavButton = ({ url, text, onClick }) => (
   <div className="nav-content cursor-pointer" onClick={onClick}>
     <img className="mr-0.5" src={`/images/icons/${url}.png`} alt={`${text} icon`} />
@@ -20,11 +26,18 @@ NavButton.propTypes = {
 };
 
 const SidebarItem = ({ url, logo, abbrv }) => (
-
-  <Link to={url} className="spectate-content justify-center" key={abbrv}>
-    <img className="team-logo" src={logo} alt={`${abbrv} logo`} />
-    <p>{abbrv.toUpperCase()}</p>
-  </Link>
+  <tr className="text-center align-middle text-3xl font-bold">
+    <td>
+      <Link to={url}>
+        <img className="team-logo" src={logo} alt={`${abbrv} logo`} />
+      </Link>
+    </td>
+    <td>
+      <Link to={url}>
+        {abbrv.toUpperCase()}
+      </Link>
+    </td>
+  </tr>
 );
 
 SidebarItem.propTypes = {
@@ -34,16 +47,8 @@ SidebarItem.propTypes = {
 };
 
 const Navbar = ({ style = {} }) => {
-  const teams = {
-    csk: "Chennai Super Kings", dc: "Delhi Capitals", gt: "Gujarat Titans",
-    kkr: "Kolkata Knight Riders", lsg: "Lucknow Supergiants", mi: "Mumbai Indians", pbks: "Punjab Kings",
-    rcb: "Royal Challengers Bangalore", rr: "Rajasthan Royals", srh: "Sunrisers Hyderabad"
-  };
-  const navigate = useNavigate()
-
-  // TODO
-  // Grey out Leaderboard Button until allowed
-  // Redirect to Dashboard if own team is selected in spectate
+  const team = localStorage.getItem("team");
+  const navigate = useNavigate();
 
   // Popup Logic
   const [showPopup, setShowPopup] = useState(false);
@@ -66,7 +71,7 @@ const Navbar = ({ style = {} }) => {
 
   return (
     <>
-      {showPopup && <Popup message="Are you sure you want to logout?" onCancel={handleClosePopup} onConfirm={performLogout} />}
+      {showPopup && <Popup message="Are you sure you want to logout?" onCancel={handleClosePopup} onConfirm={performLogout} isOK={false} />}
 
       <div className="nav-container" style={style}>
 
@@ -104,17 +109,20 @@ const Navbar = ({ style = {} }) => {
               </button>
             </div>
 
-            {Object.entries(teams)
-            .filter(([abbrv]) => abbrv.toUpperCase() !== localStorage.getItem('team'))
-            .map(([abbrv]) => ( 
-              <SidebarItem
-                key={abbrv}
-                url={`/spectate/${abbrv}`}
-                logo={`/images/teamlogo/${abbrv}.png`}
-                abbrv={abbrv}
-              />
-          ))}
-
+            <table>
+              <tbody onClick={handleCloseDrawer}>
+                {Object.entries(TEAMS)
+                  .filter(([abbrv]) => abbrv.toUpperCase() !== team)
+                  .map(([abbrv]) => (
+                    <SidebarItem
+                      key={abbrv}
+                      url={`/spectate/${abbrv}`}
+                      logo={`/images/teamlogo/${abbrv}.png`}
+                      abbrv={abbrv}
+                    />
+                  ))}
+              </tbody>
+            </table>
 
           </div>
 
@@ -125,4 +133,3 @@ const Navbar = ({ style = {} }) => {
 };
 
 export default Navbar;
-
