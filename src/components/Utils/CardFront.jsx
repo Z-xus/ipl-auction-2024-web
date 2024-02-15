6e44867a381deec645a1e0859a9d16a6ffba585c
SoulDev07 <mayankravariya@gmail.com>
2024-02-15 17:32:27 +0530

    display price at which player was sold on card

--- src/components/Utils/CardFront.jsx
+++ src/components/Utils/CardFront.jsx
@@ -1,10 +1,6 @@
 // import React from 'react'
+import { useState } from "react";
 
-/**
- * Adds appropriate suffix to Number 
- * @param {number} number - The input number to be formatted.
- * @returns {string} The formatted string with suffix.
- */
 function numberConvert(number) {
   let num = Math.abs(Number(number));
   let sign = Math.sign(num);
@@ -20,6 +16,12 @@ function numberConvert(number) {
 }
 
 const CardFront = ({ playerData, isSelected, onSelect }) => {
+  const [slot, setSlot] = useState(Number(localStorage.getItem("slot")));
+  const playerName = playerData.playerName;
+  const cardType = playerData.gender == "legendary" ? 'legend' : playerData.Elite == "E" ? 'elite' : '';
+  const flag = playerData.flag;
+  const priceIndex = playerData.isSold.findIndex(item => item.slot === slot);
+  const price = playerData.isSold[priceIndex].budget;
 
   const handleClick = () => {
     if (onSelect) {
@@ -28,19 +30,19 @@ const CardFront = ({ playerData, isSelected, onSelect }) => {
   };
 
   return (
-    <div style={{userSelect: "none"}} className={` card card-front w-32 h-44 m-1 p-1.5 rounded-xl relative overflow-hidden ${playerData.gender == "legendary" ? 'legend' : playerData.Elite == "E" ? 'elite' : ''} ${isSelected?"shadow-lg":""}`} onClick={handleClick}>
-      <img src={`/images/players/${playerData.playerName}.png`} alt={playerData.playerName} className='absolute w-full h-full object-cover' />
+    <div style={{ userSelect: "none" }} className={`card card-front w-32 h-44 m-1 p-1.5 rounded-xl relative overflow-hidden ${cardType} ${isSelected ? "shadow-lg" : ""}`} onClick={handleClick}>
+      <img src={`/images/players/${playerName}.png`} alt={playerName} className='absolute w-full h-full object-cover' />
 
       <div className="flex flex-col rounded-xl items-stretch justify-end h-full absolute bottom-0 left-0 right-0 z-10 p-2">
-        <h1 className="player-name text-center text-white">{playerData.playerName}</h1>
+        <h1 className="player-name text-center text-white">{playerName}</h1>
 
         <div className="front-stat-container">
           <div>
             <p className="text-[.7rem] leading-none stat-text"> PRICE </p>
-            <p className="text-lg leading-tight stat-text"> {numberConvert(playerData.basePrice)} </p>
+            <p className="text-lg leading-tight stat-text"> {numberConvert(price)} </p>
           </div>
 
-          <img src={`/images/flag/${playerData.flag}.png`} alt="Country Flag" className="rounded-full h-9 w-9" />
+          <img src={`/images/flag/${flag}.png`} alt="Country Flag" className="rounded-full h-9 w-9" />
         </div>
       </div>
 
