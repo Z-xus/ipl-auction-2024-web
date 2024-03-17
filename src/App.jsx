981020b5bd3cfe895d24186df1f0e28ce9e34877
SoulDev07 <mayankravariya@gmail.com>
2024-03-17 10:24:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AdminHome from './components/Admin/AdminHome'
-import './App.css'
-import LoginPage from './components/LoginPage/LoginPage'
-import DashboardPage from './components/DashboardPage/DashboardPage'
-import CalculatorPage from './components/CalculatorPage/CalculatorPage'
-import LeaderboardPage from './components/LeaderboardPage/LeaderboardPage'
-import SpectatePage from './components/SpectatePage/SpectatePage'
-import CalculatorTestPage from './components/CalculatorPage/CalculatorTestPage'
-import CalculatorTestPage2 from './components/CalculatorPage/CalculatorTestPage2'
+import { LoginPage, DashboardPage, CalculatorPage, LeaderboardPage, SpectatePage, AdminHome } from './components';
+import { CalculatorTestPage, CalculatorTestPage2 } from './components';
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -28,4 +22,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
-)
+);
--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ import CalculatorPage from "./CalculatorPage/CalculatorPage";
 import CalculatorTestPage from "./CalculatorPage/CalculatorTestPage";
 import CalculatorTestPage2 from "./CalculatorPage/CalculatorTestPage2";
 import SpectatePage from "./SpectatePage/SpectatePage"
+import AdminHome from "./Admin/AdminHome";
 
 
-export { DashboardPage, LoginPage, LeaderboardPage, CalculatorPage, CalculatorTestPage, CalculatorTestPage2, SpectatePage };
+export { DashboardPage, LoginPage, LeaderboardPage, CalculatorPage, CalculatorTestPage, CalculatorTestPage2, SpectatePage, AdminHome };
