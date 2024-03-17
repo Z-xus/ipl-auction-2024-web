0316e94e1db44f01f32d70e06248ec7eb933b447
SoulDev07 <mayankravariya@gmail.com>
2024-03-17 10:30:05 +0530

    minify build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
+  build: {
+    minify: true,
+  },
   plugins: [react(), vercel()],
   vercel: {},
 })
